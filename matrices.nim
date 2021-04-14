8d54632fe6e1982d8906a1df3e5b1ee6059e88e7
j-james <jj@j-james.me>
2021-04-13 22:09:41 -0700

    Change basic matrix operations to use a map function

--- matrices.nim
+++ matrices.nim
@@ -1,3 +1,4 @@
+import sugar
 import vectors
 
 # Matrices don't need to be more than 2D hahaha
@@ -6,36 +7,34 @@ type
   ColumnVector* = Vector
   Matrix* = seq[RowVector]
 
+## Apply an arbitrary function to every element of a matrix
+func map*(A: Matrix, op: proc(A: Matrix, i, j: int): int): Matrix =
+  result.setLen(A.len()) # At some point, I need to find out if setLen is the convention
+  for i in 0 ..< A.len():
+    result[i].setLen(A[i].len())
+    for j in 0 ..< A[i].len():
+      result[i][j] = op(A, i, j) # Note that the position is _actively_ provided
+
+## Apply an arbitrary function to every element of a matrix
+func map*(A, B: Matrix, op: (Matrix, Matrix, int, int) -> int): Matrix =
+  result.setLen(A.len())
+  for i in 0 ..< A.len():
+    result[i].setLen(A[i].len())
+    for j in 0 ..< A[i].len():
+      result[i][j] = op(A, B, i, j)
+
 ## Matrix Addition
 func `+`*(A, B: Matrix): Matrix =
-  assert A.len() == B.len()
-  for i, row in A:
-    assert A[i].len() == A[i].len()
-  for i, row in A:
-    var C: seq[int]
-    for j, element in row:
-      C.add(A[i][j] + B[i][j])
-    result.add(C)
+  return map(A, B, (a, b, i, j) => a[i][j] + b[i][j])
 
 ## Matrix Subtraction
-func `-`(A, B: Matrix): Matrix =
-  assert A.len() == B.len()
-  for i, row in A:
-    assert A[i].len() == B[i].len()
-  for i, row in A:
-    var C: seq[int]
-    for j, element in row:
-      C.add(A[i][j] - B[i][j])
-    result.add(C)
+func `-`*(A, B: Matrix): Matrix =
+  return map(A, B, (a, b, i, j) => a[i][j] - b[i][j])
 
 ## Scalar-Matrix Multiplication
-func `*`(A: int, B: Matrix): Matrix =
-  for row in B:
-    var foo: Vector
-    for element in row:
-      foo.add(A * element)
-    result.add(foo)
+func `*`*(A: int, B: Matrix): Matrix =
+  return map(B, (b, i, j) => A * b[i][j])
 
 ## Scalar-Matrix Multiplication
-func `*`(A: Matrix, B: int): Matrix =
+func `*`*(A: Matrix, B: int): Matrix =
   return B * A
