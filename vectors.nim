dc9abc51b3aca67589f4a9d64e7708da6e080e39
j-james <jj@j-james.me>
2021-04-16 02:33:55 -0700

    Minor iteration changes

--- vectors.nim
+++ vectors.nim
@@ -2,26 +2,24 @@ type Vector* = seq[int]
 
 func `+`*(a, b: Vector): Vector =
   assert a.len() == b.len(), "Dimensional mismatch - check your vectors"
-  when result is seq:
-    result.setLen(a.len)
-  for i, element in a:
+  result.setLen(a.len)
+  for i in 0 ..< a.len():
     result[i] = (a[i] + b[i])
 
 func `-`*(a, b: Vector): Vector =
   assert a.len() == b.len(), "Dimensional mismatch - check your vectors"
-  when result is seq:
-    result.setLen(a.len)
-  for i, element in a:
+  result.setLen(a.len)
+  for i in 0 ..< a.len():
     result[i] = (a[i] - b[i])
 
 func `*`*(a, b: Vector): int =
   assert a.len() == b.len(), "Dimensional mismatch - check your vectors"
-  for i, element in a:
+  for i in 0 ..< a.len():
     result += a[i] * b[i]
 
 func cross*(a, b: Vector): Vector =
-  assert a.len() == 3, "The first Vector is not three-dimensional"
-  assert b.len() == 3, "The second Vector is not three-dimensional"
+  assert a.len() == 3, "The first vector is not three-dimensional"
+  assert b.len() == 3, "The second vector is not three-dimensional"
   return @[
     (a[1]*b[2]) - (b[1]*a[2]),
     (a[2]*b[0]) - (b[2]*a[0]),
